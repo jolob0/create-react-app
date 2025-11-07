eda48010c580271b2d3c74d6fcb7c745bddbe72c
Royce <50583703+jolob0@users.noreply.github.com>
2025-11-06 23:04:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1124,13 +1124,14 @@ const App = () => {
 
 
                                                 {/* NEW ODDS BOX (Bottom position - Visible on Mobile, Hidden on Desktop) */}
-                                                <div className="sm:hidden flex bottom-0 right-0 justify-end">
-                                                    <div className="p-1 px-2 bg-indigo-600 text-white text-xs font-bold rounded-lg shadow-md flex items-center space-x-1">
+                                                /*<div className="sm:hidden flex bottom-0 right-0 justify-end">*/
+                                                    /*<div className="p-1 px-2 bg-indigo-600 text-white text-xs font-bold rounded-lg shadow-md flex items-center space-x-1">*/
+                                                    <div className="sm:hidden flex absolute top-0 right-0 p-1 bg-indigo-600 text-white text-xs font-bold rounded-tr-xl rounded-bl-lg shadow-md items-center space-x-1">
                                                         <span className="uppercase font-extrabold px-1 bg-white text-indigo-600 rounded-sm">AWAY</span>
                                                         <span className="opacity-80">ML:</span> 
                                                         <span>{game.awayOdds || 'N/A'}</span>
                                                     </div>
-                                                </div>
+                                                /*</div>*/
 
                                                 {/* OLD ODDS BOX (Desktop position - Hidden on Mobile, Visible on Desktop) */}
                                                 <div className="hidden sm:flex absolute top-0 right-0 p-1 bg-indigo-600 text-white text-xs font-bold rounded-tr-xl rounded-bl-lg shadow-md items-center space-x-1">
